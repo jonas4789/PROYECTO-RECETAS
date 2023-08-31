e532e43289c5fc811ad981b60aa099711d0f10eb
renatomgg <renato.mayeregger@gmail.com>
2023-08-31 16:53:57 -0400

    cambios en la funcion mostrarReceta

--- functions.js
+++ functions.js
@@ -1,7 +1,13 @@
 //llama a la funcion ocultar al cargar la pagina, para ocultar todas las recetas
 document.addEventListener("DOMContentLoaded", function() {
+    iniciar();
     ocultar(); 
 });
+
+function iniciar(){
+    //mostrar mensaje inicial
+    document.getElementById("mensajeInicial").innerHTML = "Elije una receta para comenzar";
+}
 
 function ocultar(){
     // Obtiene todas las recetas a traves de sus clases
@@ -11,9 +17,12 @@ function ocultar(){
     for (let i = 0; i < recetas.length; i++) {
         recetas[i].style.display = "none";
     }
+    
 };
 
 function mostrarReceta(id){
+    //ocultar mensaje inicial
+    document.getElementById("mensajeInicial").style.display = "none"
 
     // Obtiene todos los botones a traves de sus clases
     const botones = document.getElementsByClassName("w3-button w3-round-xlarge w3-border");
@@ -25,4 +34,11 @@ function mostrarReceta(id){
 
     // Cambiar el color del botón seleccionado
     document.getElementById(id).style.background = "orange";
+
+    //oculta todas las recetas
+    ocultar(); 
+    
+    //Muestra la receta
+    document.getElementById(`R${id}`).style.display = "block"
+
 }
